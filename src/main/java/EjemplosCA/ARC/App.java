b209db1add7a72652f8c9ed7192080e833cf552a
santiagobp24 <santiago83iti@gmail.com>
2022-03-18 00:47:12 -0500

    Primer

---
+++ src/main/java/EjemplosCA/ARC/App.java
@@ -0,0 +1,13 @@
+package EjemplosCA.ARC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
